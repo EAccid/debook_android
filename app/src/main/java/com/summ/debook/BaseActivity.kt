69e28a323df57c8d7d406336514b45a9042e02d0
olena.brygynets <obr@ciklum.com>
2018-05-02 17:05:16 +0300

    Added retrofit dependencies.

---
+++ app/src/main/java/com/summ/debook/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.summ.debook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+}
